5d1eefa13df76096121dfe7b3cd34874bbb42471
ret7020 <ret7020@gmail.com>
2023-09-01 21:53:36 +0300

    udp poc

--- udp/master.py
+++ udp/master.py
@@ -1,14 +1,15 @@
 import socket
-import time
+import subprocess
+import json
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-server.settimeout(0.2)
-server.bind(("", 44444))
-message = b"your very important message"
-while True:
-    server.sendto(message, (192.168.1.11, 37020))
-    print("message sent!")
-    time.sleep(1)
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    server.settimeout(0.2)
+    server.bind(("", 44444))
+    message = b"your very important message"
+    while True:
+        x, y = list(map(lambda c: int(int(c) / 1.5), subprocess.check_output(["hyprctl", "cursorpos"]).decode("utf-8").strip().split(", ")))
+        payload = json.dumps(
+            {"action": "cursor", "x": x, "y": y}
+        )
+        server.sendto(payload.encode("utf-8"), ("192.168.1.11", 37020))
--- udp/slave.py
+++ udp/slave.py
@@ -1,8 +1,18 @@
 import socket
+import json
+from Xlib import display
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client.bind(("", 37020))
-while True:
-    data, addr = sock.recvfrom(1024)
-    print(data)
+display = display.Display()
+screen = display.screen()
+root = screen.root
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client.bind(("", 37020))
+    while True:
+        data, addr = client.recvfrom(1024)
+        data = json.loads(data.decode("utf-8"))
+        if data["action"] == "cursor":
+            x, y = data["x"], data["y"]
+            root.warp_pointer(x, y)
+            display.sync()
