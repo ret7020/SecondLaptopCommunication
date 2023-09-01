6ec243ae58b0c13ff9bac058dbb1fbd6f78d1c57
ret7020 <ret7020@gmail.com>
2023-09-01 21:22:40 +0300

    TCP based remote cursor proof of concept

--- RemoteLaptop/client.py
+++ RemoteLaptop/client.py
@@ -6,15 +6,18 @@ import json
 COEFF = 1.5
 SERV = "192.168.1.11"
 
-
+last = time.time()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((SERV, 9999))
     while True:
         x, y = list(map(lambda c: int(int(c) / 1.5), subprocess.check_output(["hyprctl", "cursorpos"]).decode("utf-8").strip().split(", ")))
-        payload = f"{x},{y}".encode("utf-8")
-        print(payload)
+        payload = json.dumps(
+            {"action": "cursor", "x": x, "y": y}
+        )
+        payload += "#"
+        payload = payload.encode("utf-8")
         s.sendall(payload)
-        time.sleep(0.01)
+        #time.sleep(0.001)
 
 
 
